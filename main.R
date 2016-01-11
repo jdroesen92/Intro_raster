361716de4dea64b6a65555e96ab6c39f92e8a40d
jdroesen92 <jan.droesen@wur.nl>
2016-01-11 09:18:40 +0100

    Final map

--- main.R
+++ main.R
@@ -10,7 +10,8 @@ getwd()
 
 
 source('R/calcNDVI.R')
-source('')
+source('R/difNDVI.R')
+source('R/calcNDVI.R')
 
 #downloading the data
 download.file(url='https://www.dropbox.com/s/i1ylsft80ox6a32/LC81970242014109-SC20141230042441.tar.gz?dl=0', destfile='Wageningen8.tar', method='wget')
@@ -25,20 +26,9 @@ LS2014 <- list.files(pattern = glob2rx('LC8*.tif'), full.names = TRUE)
 rstack1990 <- stack(LS1990)
 rstack2014 <- stack(LS2014)
 
-#give same extent
-#LS2014_newex <-setExtent(rstack2014, rstack1990, snap=TRUE)
-#extent (rstack2014) <- rstack1990
-#LS2014_newex
-#rstack2014
-#make raster bricks
-
 #Calculate NDVIs  Landsat 8(2014): Red = band 4 (layer 5) and NIR = band 5 (layer 6)
 #and Landsat 5(1990): Red = band 3 (layer 6) and NIR = band 4 (layer 7)
-source('home/user/git/geoScripting/NDVIdiff/R/calcNDVI.R')
-ndvOver <- function(x, y) {
-	ndvi <- (y - x) / (x + y)
-	return(ndvi)
-}
+
 ndvi1990 <- overlay(x=rstack1990[[6]], y=rstack1990[[7]], fun=ndvOver)
 ndvi2014 <- overlay(x=rstack2014[[5]], y=rstack2014[[6]], fun=ndvOver)
 
@@ -49,10 +39,6 @@ plot(ndvi2014)
 #extract the cloud mask from layers (layer 1)
 fmask1990 <- rstack1990[[1]]
 fmask2014 <- rstack2014[[1]]
-cloud2NA <- function(x, y){
-x[y != 0] <- NA
-return(x)
-}
 
 cloudfree1990 <- cloud2NA(ndvi1990, fmask1990)
 cloudfree2014 <- cloud2NA(ndvi2014, fmask2014)
@@ -64,13 +50,10 @@ cloudfree2014 <- cloud2NA(ndvi2014, fmask2014)
 plot(cloudfree1990)
 plot(cloudfree2014)
 
-#Substraction two NDVI layers of now and thirty years ago
-NDVIdiff <- function(x, y) {
-	ndvidif <- (x - y)
-	return(ndvidif)
-}
+#Substraction of 1990 from 2014
 
 NDVIdiff <- NDVIdiff(cloudfree2014,cloudfree1990)
-#Output
-plot(NDVIdiff)
-plot(NDVIdiff, col = gray.colors(10, start = -1.5, end = 1.5, gamma = 2.2, alpha = NULL))
+#plot with in yellow small changes, in red a decrease in NDVI values and in green an increase of NDVI values
+breakpoints <- c(-1.5, -0.2, 0.2, 1.5)
+colors <- c("red", "yellow", "green")
+plot(NDVIdiff, breaks=breakpoints, col = colors)
