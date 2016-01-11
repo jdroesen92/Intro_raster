c9d3cccae61283bf2e0c1c4f6daded2fc0f6445b
jdroesen92 <jan.droesen@wur.nl>
2016-01-11 09:14:32 +0100

    Visualization of changes

--- R/main.R
+++ R/main.R
@@ -1,79 +1,68 @@
+#CarJAn
+#Careli Caballero, Jan Droesen
+# 08/01/2016
+
+#LIbraries required
 library(rgdal)
 library(raster)
-#check if working directory VegetationChangeWageningen
+#check if we are working in the correct directory
 getwd()
 
 #downloading the data
- <- download.file(url='https://www.dropbox.com/s/i1ylsft80ox6a32/LC81970242014109-SC20141230042441.tar.gz?dl=0', destfile='Wageningen8.tar', method='wget')
+download.file(url='https://www.dropbox.com/s/i1ylsft80ox6a32/LC81970242014109-SC20141230042441.tar.gz?dl=0', destfile='Wageningen8.tar', method='wget')
 untar(tarfile='Wageningen8.tar')
 download.file(url='https://www.dropbox.com/s/akb9oyye3ee92h3/LT51980241990098-SC20150107121947.tar.gz?dl=0', destfile='Wageningen5.tar', method='wget')
 untar(tarfile='Wageningen5.tar')
 
 #check the file names, and resolution/projection. And check whether resolution/extent/projection is the same
-#if not the same  -> resample, reproject, intersect
 GDALinfo("LC81970242014109LGN00_sr_band4.tif")
 GDALinfo("LT51980241990098KIS00_sr_band3.tif")
 
 # create list of files for the two images
-Landsat1990 <- list.files(pattern = glob2rx('LT5*.tif'), full.names = TRUE)
-Landsat2014 <- list.files(pattern = glob2rx('LC8*.tif'), full.names = TRUE)
+LS1990 <- list.files(pattern = glob2rx('LT5*.tif'), full.names = TRUE)
+LS2014 <- list.files(pattern = glob2rx('LC8*.tif'), full.names = TRUE)
+#make raster stacks
+rbrick1990 <- brick(LS1990)
+rstack2014 <- stack(LS2014)
+rstack1990
+rstack2014
+#give same extent
+LS2014_newex <-setExtent(rstack2014, rstack1990, snap=TRUE)
+extent (rstack2014) <- rstack1990
+LS2014_newex
+rstack2014
+#make raster bricks
+rbrick1990 <- brick(LS2014_newex)
+rbrick2014 <- brick(rstack2014)
+rbrick1990
+rbrick2014
 
-stack()
-
-# make raster layers
-rasterlayers1990 <- lapply(Landsat1990, raster)
-rasterlayers2014 <- lapply(Landsat2014, raster)
-rasterlayers2014
-rasterlayers1990
-<- stack()
-stack()
-
-#Calculate NDVI  Landsat 8: red = band 4 and NIR = band 5 Landsat 5/7: red 3 and nir 4
-ndvOver <- function(x, y) {
-	ndvi <- (y - x) / (x + y)
-	return(ndvi)
-}
-NDVI2014 <- ndvOver(rasterlayers2014[[5]], rasterlayers2014[[6]])
-NDVI1990 <- ndvOver(rasterlayers1990[[6]], rasterlayers1990[[7]])	
-
-
-	5 and 6 for 2014
-6 and 7 for 1990
-#give the same extent for both images
-lapply(rasterlayers2014, intersect(rasterlayers1990, rasterlayers2014))
-for i 1:nlayers(rasterlayers2014)
-	intersect(rasterlayers[1], rasterlayers[i])
+#Calculate NDVIs  Landsat 8(2014): Red = band 4 (layer 5) and NIR = band 5 (layer 6) 
+#and Landsat 5(1990): Red = band 3 (layer 6) and NIR = band 4 (layer 7)
 
 
+source(calculateNDVI)
+ndvOver(1990l3, 1990l4)
+ndvOver(2014l3, 2014l4)
 
-
-#ndvi3 <- overlay(x=gewata[[3]], y=gewata[[4]], fun=ndvOver)
-
-
-
-
-#plot both NDVI maps
-
-#cloud mask from layer 7?...
-cloud1 <- Wageningen2015[[7]]
-cloud[cloud==0] <- NA
-#
-plotRGB(tahiti, 3,4,5)
-plot(cloud, add = TRUE, legend = FALSE)
-fmask <- Wageningen2015[[7]]
-Wageningen.. <- dropLayer(Wageningen2015, 7)
-Wageningen.. [fmask != 0]<- NA 
+#extract the cloud mask from layers (layer 1)
 cloud2NA <- function(x, y){
 	x[y != 0] <- NA
 	return(x)
 }
-plotRGB(tahitiCloudFree, 3,4,5)
+cloudfree1990 <- cloud2NA(rbrick1990,rbrick1990[[1]])
+cloudfree2014 <- cloud2NA(rbrick2014,rbrick2014[[1]])
+plot(cloudfree1990)
+plot(cloudfree2014)
 
-#to programmatically extract the files from the archive
-#untar()
+NDVI1990 <- ndvOver(cloudfree1990[[6]], cloudfree1990[[7]])	
+NDVI2014 <- ndvOver(cloudfree2014[[5]], cloudfree2014[[6]])
+#plot both NDVI images
+plot(NDVI1990)
+plot(NDVI2014)
 
-getwd()
 #Substraction two NDVI layers of now and thirty years ago
-difference <- calc(layer1-layer
-									 #Output
-									 plot(difference)
+NDVIdiff <-(NDVI2014-NDVI1990)
+#Output
+plot(NDVIdiff)
+									 
